edbfb9dfcfe8d8770250bee15c93d530368eea4d
IrsyadMuahamad <irsyadmuhamad158@gmail.com>
2022-06-15 20:24:14 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/uts_191011402916_aplikasi_sederhana/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.uts_191011402916_aplikasi_sederhana
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
